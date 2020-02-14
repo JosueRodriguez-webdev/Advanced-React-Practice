78d0c03ff43d5138719d7bdbcb80489a67aa2aa1
Josue Rodriguez <smexyjosue@gmail.com>
2020-02-13 16:17:27 -0800

    Initialize Repo and change all function components to class components
    Initial commit - added dependencies and changed app function component
    to class component.

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,42 @@
-import React from 'react';
+import React from "react";
 
-import './App.css';
+import axios from "axios";
 
-function App() {
-  
+import "./App.css";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      players: []
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get(`http://localhost:5000/api/footballers`)
+      .then((res) => {
+        // console.table(`Success! you got data`, res.data);
+        this.setState({ players: res.data });
+        // console.log(Array.isArray(this.state.players));
+      })
+      .then((err) => {
+        console.log(`ERROR, check again`, err);
+      });
+  }
+
+  render() {
     return (
-    <div className="App">
-      <h1>Happy building <span role="img" aria-label="Smiling emoji">😃</span></h1>
-    </div>
-  );
+      <div className="App">
+        <h1>
+          Happy building{" "}
+          <span role="img" aria-label="Smiling emoji">
+            😃
+          </span>
+        </h1>
+      </div>
+    );
+  }
 }
 
 export default App;
